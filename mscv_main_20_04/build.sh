fc6a6aba1e3f6b9368dcfb4f4c0ce39f3d61a091
leonana69 <chengj0803@gmail.com>
2023-02-07 22:59:20 -0500

    update mscv_main

--- mscv_main_20_04/build.sh
+++ mscv_main_20_04/build.sh
@@ -1,7 +1,10 @@
-docker rm -f mscv_focal &>/dev/null
+docker stop mscv_main
+docker image rm -f mscv_main:20.04
+docker rm -f mscv_main &>/dev/null
+docker build -t mscv_main:20.04 .
 # Create a new container
 docker run -td --privileged --net=host --ipc=host \
-    --name="mscv_focal" \
+    --name="mscv_main" \
     --gpus=all \
     -e "DISPLAY=$DISPLAY" \
     -e "QT_X11_NO_MITSHM=1" \
@@ -10,4 +13,4 @@ docker run -td --privileged --net=host --ipc=host \
     -e ROS_IP=127.0.0.1 \
     --cap-add=SYS_PTRACE \
     -v /etc/group:/etc/group:ro \
-    mscv:20.04 bash
+    mscv_main:20.04 bash
--- ubuntu_20.04/build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t mscv:20.04 .
