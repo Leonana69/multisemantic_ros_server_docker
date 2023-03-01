893adcf35b426addc2e2a6c957a1ed4b1f17ea8b
leonana69 <chengj0803@gmail.com>
2023-03-01 11:26:40 -0500

    update

--- mscv_http_20_04/build.sh
+++ mscv_http_20_04/build.sh
@@ -6,13 +6,8 @@ docker build -t mscv_http:20.04 .
 docker run -td --privileged --net=host --ipc=host \
     --name="mscv_http" \
     --gpus=all \
-    -e "DISPLAY=$DISPLAY" \
-    -e "QT_X11_NO_MITSHM=1" \
-    -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    -e "XAUTHORITY=$XAUTH" \
     -e ROS_IP=127.0.0.1 \
     --cap-add=SYS_PTRACE \
-    -v /etc/group:/etc/group:ro \
     -p 50001:50001 \
     mscv_http:20.04 \
-    bash -it -c "cd ~/catkin_ws && roslaunch mscv_http_service run.launch"
+    bash -it -c "cd /root/catkin_ws && roslaunch mscv_http_service run.launch"
